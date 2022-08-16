d453d73ec3706d0f9de2804a713c312d7961df57
Nyanism <ojjstuff@gmail.com>
2022-08-16 16:51:37 +0800

    Implement hover effect
    The square grids will now change colour whenever the user moves their
    mouse over it.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for (let ii = 0; ii < 16; ii++) {
   const gridRow = document.createElement("div");
-  gridRow.setAttribute("class", "grid-row");
+  gridRow.classList.add("grid-row");
   document.querySelector(".grid").appendChild(gridRow);
 }
 
@@ -10,7 +10,10 @@ const rows = document.querySelectorAll(".grid-row");
 for(let hh = 0; hh < rows.length; hh++) {
   for(let jj=0; jj < 16; jj++) {
     const grid = document.createElement("div");
-    grid.setAttribute("class", "grid-square");
+    grid.classList.add("grid-square");
+    grid.addEventListener("mouseover", () => {
+      grid.classList.add("hover");
+    })
     rows[hh].appendChild(grid);
   }
 }
