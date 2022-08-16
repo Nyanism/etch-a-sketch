afe667ea3e7f56b7b898d038a0ec6997522c23f2
Nyanism <ojjstuff@gmail.com>
2022-08-16 17:38:17 +0800

    Implement grid change function
    Users can now input the desired number of squares per side and a new
    grid will be generated according to the user's specification.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-for (let ii = 0; ii < 16; ii++) {
-  const gridRow = document.createElement("div");
-  gridRow.classList.add("grid-row");
-  document.querySelector(".grid").appendChild(gridRow);
-}
+function createGrid(width) {
+  document.querySelector(".grid").remove();
 
-const grid = document.createElement("div");
-grid.setAttribute("class", "grid-square");
-const rows = document.querySelectorAll(".grid-row");
-for(let hh = 0; hh < rows.length; hh++) {
-  for(let jj=0; jj < 16; jj++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid-square");
-    grid.addEventListener("mouseover", () => {
-      grid.classList.add("hover");
-    })
-    rows[hh].appendChild(grid);
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+  document.querySelector("body").appendChild(grid);
+
+  for (let ii = 0; ii < width; ii++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    document.querySelector(".grid").appendChild(gridRow);
+  }
+
+  const gridSquare = document.createElement("div");
+  gridSquare.setAttribute("class", "grid-square");
+  const rows = document.querySelectorAll(".grid-row");
+  for (let hh = 0; hh < rows.length; hh++) {
+    for (let jj = 0; jj < width; jj++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid-square");
+      grid.addEventListener("mouseover", () => {
+        grid.classList.add("hover");
+      });
+      rows[hh].appendChild(grid);
+    }
   }
 }
 
+let width = 16;
+createGrid(width);
+
+const newGrid = document.querySelector(".new-grid");
+newGrid.addEventListener("click", () => {
+  width = parseInt(prompt("How many squares per side would you like?"));
+  createGrid(width);
+});
